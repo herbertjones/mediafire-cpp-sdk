4bb224b35f6db5011f7f3e6b76e548fc6b4077fb
Zachary Marquez <zachary.marquez@mediafire.com>
2014-09-28 22:28:06 -0500

    ENH: 26316: Added extra debug output to determine why certificate verification errors happen.

--- src/mediafire_sdk/http/detail/state_ssl_handshake.hpp
+++ src/mediafire_sdk/http/detail/state_ssl_handshake.hpp
@@ -22,6 +22,109 @@ namespace mf {
 namespace http {
 namespace detail {
 
+namespace {
+
+#define CASE(X) case X: error_msg=#X; break;
+void fprint_openssl_error(FILE* file, int error)
+{
+    fprintf(file, "Error: ");
+    std::string error_msg;
+    switch (error)
+    {
+        CASE(X509_V_OK)
+        CASE(X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT)
+        CASE(X509_V_ERR_UNABLE_TO_GET_CRL)
+        CASE(X509_V_ERR_UNABLE_TO_DECRYPT_CERT_SIGNATURE)
+        CASE(X509_V_ERR_UNABLE_TO_DECRYPT_CRL_SIGNATURE)
+        CASE(X509_V_ERR_UNABLE_TO_DECODE_ISSUER_PUBLIC_KEY)
+        CASE(X509_V_ERR_CERT_SIGNATURE_FAILURE)
+        CASE(X509_V_ERR_CRL_SIGNATURE_FAILURE)
+        CASE(X509_V_ERR_CERT_NOT_YET_VALID)
+        CASE(X509_V_ERR_CERT_HAS_EXPIRED)
+        CASE(X509_V_ERR_CRL_NOT_YET_VALID)
+        CASE(X509_V_ERR_CRL_HAS_EXPIRED)
+        CASE(X509_V_ERR_ERROR_IN_CERT_NOT_BEFORE_FIELD)
+        CASE(X509_V_ERR_ERROR_IN_CERT_NOT_AFTER_FIELD)
+        CASE(X509_V_ERR_ERROR_IN_CRL_LAST_UPDATE_FIELD)
+        CASE(X509_V_ERR_ERROR_IN_CRL_NEXT_UPDATE_FIELD)
+        CASE(X509_V_ERR_OUT_OF_MEM)
+        CASE(X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT)
+        CASE(X509_V_ERR_SELF_SIGNED_CERT_IN_CHAIN)
+        CASE(X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY)
+        CASE(X509_V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE)
+        CASE(X509_V_ERR_CERT_CHAIN_TOO_LONG)
+        CASE(X509_V_ERR_CERT_REVOKED)
+        CASE(X509_V_ERR_APPLICATION_VERIFICATION)
+        default:
+            error_msg = "Unknown error: " + mf::utils::to_string(error);
+    }
+    fprintf(file, "%s\n", error_msg.c_str());
+}
+#undef CASE
+
+class DebugCertificateVerifier
+{
+public:
+    DebugCertificateVerifier(std::string hostname) :
+        hostname_(std::move(hostname))
+    {
+        // Empty
+    }
+
+    bool operator()(bool preVerified, boost::asio::ssl::verify_context& ctx)
+    {
+        X509_STORE_CTX* x509_ctx = ctx.native_handle();
+
+#ifdef OUTPUT_DEBUG
+        X509* cert = x509_ctx->cert;
+        fprintf(stdout, "Validating Certificate issued by:\n");
+        X509_NAME_print_ex_fp(
+                stdout,
+                cert->cert_info->issuer,
+                2,
+                XN_FLAG_RFC2253
+            );
+        fprintf(stdout, "\n");
+        fprintf(stdout, "Issued to:\n");
+        X509_NAME_print_ex_fp(
+                stdout,
+                cert->cert_info->subject,
+                2,
+                XN_FLAG_RFC2253
+            );
+        fprintf(stdout, "\n");
+#endif
+
+        bool returnValue =
+            boost::asio::ssl::rfc2818_verification(hostname_)(preVerified, ctx);
+
+        if ( X509_STORE_CTX_get_error(x509_ctx) != 0 )
+        {
+            fprintf(stdout, "Verification failed!\n");
+            X509* failed_cert = X509_STORE_CTX_get_current_cert(x509_ctx);
+            if ( failed_cert != nullptr )
+            {
+                fprintf(stdout, "At certificate:\n");
+                X509_NAME_print_ex_fp(
+                        stdout,
+                        failed_cert->cert_info->subject,
+                        2,
+                        XN_FLAG_RFC2253
+                    );
+                fprintf(stdout, "\n");
+            }
+            fprint_openssl_error(stdout, X509_STORE_CTX_get_error(x509_ctx));
+        }
+
+        return returnValue;
+    }
+
+private:
+    std::string hostname_;
+};
+
+}  // anonymous namespace
+
 class SslConnectData
 {
 public:
@@ -93,8 +196,8 @@ public:
 
         // Properly walk the certificate chain.
         ssl_socket->set_verify_callback(
-                boost::asio::ssl::rfc2818_verification(url->host())
-                );
+                DebugCertificateVerifier(url->host())
+            );
 
         // Must prime timeout for async actions.
         auto race_preventer = fsm.SetAsyncTimeout("ssl handshake",
