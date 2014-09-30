d0e89564008f89678edaff7a2e884a4b715cb85a
Herbert Jones <herbert.jones@mediafire.com>
2014-09-30 15:46:40 -0500

    COMP 26143 Remove warnings

---
+++ src/mediafire_sdk/uploader/detail/transition_upload.cpp
@@ -0,0 +1,50 @@
+/**
+ * @file transition_upload.cpp
+ * @author Herbert Jones
+ * @copyright Copyright 2014 Mediafire
+ */
+#include "transition_upload.hpp"
+
+namespace mf {
+namespace uploader {
+namespace detail {
+namespace upload_transition {
+
+std::string AsDateTime(std::time_t datetime)
+{
+    boost::posix_time::ptime ptime_(boost::posix_time::from_time_t(datetime));
+    return boost::posix_time::to_iso_extended_string(ptime_) + "Z";
+}
+
+std::string AssembleQuery(
+        const std::map< std::string, std::string > & query_map
+    )
+{
+    using mf::utils::url::get_parameter;
+
+    std::vector<std::string> query_pieces;
+    query_pieces.reserve( query_map.size() );
+    for ( const auto & pair : query_map )
+    {
+        if ( pair.second.empty() )
+        {
+            query_pieces.push_back(get_parameter::Encode(pair.first));
+        }
+        else
+        {
+            std::string piece = (pair.first);
+            piece += "=";
+            piece += get_parameter::Encode(pair.second);
+            query_pieces.push_back(piece);
+        }
+    }
+
+    std::string url = "?";
+    url += boost::join(query_pieces, "&");
+    return url;
+}
+
+}  // namespace upload_transition
+}  // namespace detail
+}  // namespace uploader
+}  // namespace mf
--- src/mediafire_sdk/uploader/detail/transition_upload.hpp
+++ src/mediafire_sdk/uploader/detail/transition_upload.hpp
@@ -31,39 +31,17 @@
 #include "mediafire_sdk/utils/string.hpp"
 #include "mediafire_sdk/utils/url_encode.hpp"
 
-namespace {
-std::string AsDateTime(std::time_t datetime)
-{
-    boost::posix_time::ptime ptime_(boost::posix_time::from_time_t(datetime));
-    return boost::posix_time::to_iso_extended_string(ptime_) + "Z";
-}
+namespace mf {
+namespace uploader {
+namespace detail {
+namespace upload_transition {
+
+std::string AsDateTime(std::time_t datetime);
+
 std::string AssembleQuery(
         const std::map< std::string, std::string > & query_map
-    )
-{
-    using mf::utils::url::get_parameter;
+    );
 
-    std::vector<std::string> query_pieces;
-    query_pieces.reserve( query_map.size() );
-    for ( const auto & pair : query_map )
-    {
-        if ( pair.second.empty() )
-        {
-            query_pieces.push_back(get_parameter::Encode(pair.first));
-        }
-        else
-        {
-            std::string piece = (pair.first);
-            piece += "=";
-            piece += get_parameter::Encode(pair.second);
-            query_pieces.push_back(piece);
-        }
-    }
-
-    std::string url = "?";
-    url += boost::join(query_pieces, "&");
-    return url;
-}
 class TargetUrlVisitor : public boost::static_visitor<>
 {
 public:
@@ -86,13 +64,6 @@ public:
 private:
     std::map< std::string, std::string > & query_map_;
 };
-
-}  // namespace
-
-namespace mf {
-namespace uploader {
-namespace detail {
-namespace upload_transition {
 
 class UploadPostDataPipe :
     public mf::http::PostDataPipeInterface
