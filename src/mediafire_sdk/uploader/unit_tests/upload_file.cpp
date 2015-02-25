b005b414335d5bd75605d88f1e7fadede11fb015
Herbert Jones <herbert.jones@mediafire.com>
2015-02-25 10:36:54 -0600

    clang-format file

--- src/mediafire_sdk/uploader/unit_tests/upload_file.cpp
+++ src/mediafire_sdk/uploader/unit_tests/upload_file.cpp
@@ -16,8 +16,8 @@
 #include "boost/asio.hpp"
 #include "boost/asio/ssl.hpp"
 #ifdef BOOST_ASIO_SEPARATE_COMPILATION
-#  include "boost/asio/impl/src.hpp"  // Define once in program
-#  include "boost/asio/ssl/impl/src.hpp"  // Define once in program
+#include "boost/asio/impl/src.hpp"      // Define once in program
+#include "boost/asio/ssl/impl/src.hpp"  // Define once in program
 #endif
 #include "boost/bind.hpp"
 #include "boost/filesystem.hpp"
@@ -37,39 +37,33 @@ namespace us = mf::uploader::upload_state;
 class StatusVisitor : public boost::static_visitor<>
 {
 public:
-    StatusVisitor(
-            asio::io_service & io_service,
-            int id,
-            const int files_expected,
-            int * files_complete,
-            std::map<int, std::string> * summary_map
-    ) :
-        io_service_(io_service),
-        files_expected_(files_expected),
-        files_complete_(files_complete),
-        summary_map_(summary_map),
-        id_(id)
+    StatusVisitor(asio::io_service & io_service,
+                  int id,
+                  const int files_expected,
+                  int * files_complete,
+                  std::map<int, std::string> * summary_map)
+            : io_service_(io_service),
+              files_expected_(files_expected),
+              files_complete_(files_complete),
+              summary_map_(summary_map),
+              id_(id)
     {
         if (files_expected > 0)
         {
             std::ostringstream ss;
-            ss << "[" << (id+1) << "] ";
+            ss << "[" << (id + 1) << "] ";
             id_str_ = ss.str();
         }
     }
 
-    void operator()(us::EnqueuedForHashing &) const
-    {
-    }
+    void operator()(us::EnqueuedForHashing &) const {}
 
     void operator()(us::Hashing &) const
     {
         std::cout << id_str_ << "Hashing file." << std::endl;
     }
 
-    void operator()(us::EnqueuedForUpload &) const
-    {
-    }
+    void operator()(us::EnqueuedForUpload &) const {}
 
     void operator()(us::Uploading & status) const
     {
@@ -85,10 +79,10 @@ public:
     {
         const auto & ec = status.error_code;
         std::cout << id_str_ << "Error: " << ec.message() << std::endl;
-        AddDebug( "Received error: " + ec.message() );
-        AddDebug( "Error type: " + std::string(ec.category().name()));
-        AddDebug( "Error value: " + mf::utils::to_string(ec.value()));
-        AddDebug( "Description: " + status.description);
+        AddDebug("Received error: " + ec.message());
+        AddDebug("Error type: " + std::string(ec.category().name()));
+        AddDebug("Error value: " + mf::utils::to_string(ec.value()));
+        AddDebug("Description: " + status.description);
 
         *files_complete_ += 1;
         if (*files_complete_ == files_expected_)
@@ -98,9 +92,9 @@ public:
     void operator()(us::Complete & status) const
     {
         std::cout << id_str_ << "Upload complete." << std::endl;
-        AddDebug( "Upload complete.");
-        AddDebug( "New quickkey: " + status.quickkey);
-        AddDebug( "Filename: " + status.filename);
+        AddDebug("Upload complete.");
+        AddDebug("New quickkey: " + status.quickkey);
+        AddDebug("Filename: " + status.filename);
 
         *files_complete_ += 1;
         if (*files_complete_ == files_expected_)
@@ -108,10 +102,7 @@ public:
     }
 
 private:
-    void AddDebug(std::string str) const
-    {
-        (*summary_map_)[id_] += str + "\n";
-    }
+    void AddDebug(std::string str) const { (*summary_map_)[id_] += str + "\n"; }
 
     asio::io_service & io_service_;
     const int files_expected_;
@@ -122,21 +113,19 @@ private:
     std::string id_str_;
 };
 
-void ShowUsage(
-        const char * filename,
-        const po::options_description & visible
-    )
+void ShowUsage(const char * filename, const po::options_description & visible)
 {
     std::cout << "Usage: " << filename << " [options]"
-        " -u USERNAME"
-        " -p PASSWORD"
-        " FILES\n";
+                                          " -u USERNAME"
+                                          " -p PASSWORD"
+                                          " FILES\n";
     std::cout << visible << "\n";
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    try {
+    try
+    {
         std::string directory_path;
         std::string folderkey;
         std::string password;
@@ -145,6 +134,7 @@ int main(int argc, char *argv[])
         std::string username;
 
         po::options_description visible("Allowed options");
+        /* clang-format off */
         visible.add_options()
             ("folderkey"     , po::value<std::string>(&folderkey)      , "Folderkey to the directory where to upload")
             ("help,h"        ,                                           "Show this message.")
@@ -155,10 +145,13 @@ int main(int argc, char *argv[])
             ("replace,r"     ,                                           "Replace file if one exists already with the same name.")
             ("autorename,a"  ,                                           "Rename the file if it exists already.")
             ;
+        /* clang-format on */
 
         po::options_description hidden("Hidden options");
-        hidden.add_options()
-            ("upload_file_path", po::value<std::vector<std::string>>(&upload_file_path), "upload_file_path");
+        hidden.add_options()(
+                "upload_file_path",
+                po::value<std::vector<std::string>>(&upload_file_path),
+                "upload_file_path");
 
         po::positional_options_description p;
         p.add("upload_file_path", -1);
@@ -167,22 +160,24 @@ int main(int argc, char *argv[])
         cmdline_options.add(visible).add(hidden);
 
         po::variables_map vm;
-        try {
-            po::store(po::command_line_parser(argc, argv).
-                options(cmdline_options).positional(p).run(), vm);
+        try
+        {
+            po::store(po::command_line_parser(argc, argv)
+                              .options(cmdline_options)
+                              .positional(p)
+                              .run(),
+                      vm);
             po::notify(vm);
         }
-        catch(boost::program_options::error & err)
+        catch (boost::program_options::error & err)
         {
             std::cout << "Error: " << err.what() << std::endl;
             ShowUsage(argv[0], visible);
             return 1;
         }
 
-        if (     vm.count("help")
-            || ! vm.count("upload_file_path")
-            || ! vm.count("username")
-            || ! vm.count("password") )
+        if (vm.count("help") || !vm.count("upload_file_path")
+            || !vm.count("username") || !vm.count("password"))
         {
             ShowUsage(argv[0], visible);
             return 0;
@@ -207,19 +202,19 @@ int main(int argc, char *argv[])
 
             // Handle session token failures.
             stm.SetSessionStateChangeCallback(
-                [&io_service](mf::api::SessionState state)
-                {
-                    if (boost::get<mf::api::session_state::CredentialsFailure>(
-                            &state))
+                    [&io_service](mf::api::SessionState state)
                     {
-                        std::cout << "Username or password incorrect."
-                        << std::endl;
-                        io_service.stop();
-                    }
-                });
+                        if (boost::get<mf::api::session_state::
+                                               CredentialsFailure>(&state))
+                        {
+                            std::cout << "Username or password incorrect."
+                                      << std::endl;
+                            io_service.stop();
+                        }
+                    });
 
-            stm.SetLoginCredentials( mf::api::credentials::Email{
-                username, password } );
+            stm.SetLoginCredentials(
+                    mf::api::credentials::Email{username, password});
 
             mf::uploader::UploadManager um(&stm);
 
@@ -241,25 +236,25 @@ int main(int argc, char *argv[])
                 if (vm.count("replace"))
                 {
                     request.SetOnDuplicateAction(
-                        mf::uploader::OnDuplicateAction::Replace);
+                            mf::uploader::OnDuplicateAction::Replace);
                 }
 
                 if (vm.count("autorename"))
                 {
                     request.SetOnDuplicateAction(
-                        mf::uploader::OnDuplicateAction::AutoRename);
+                            mf::uploader::OnDuplicateAction::AutoRename);
                 }
 
                 um.Add(request,
-                    [&io_service, files_to_upload, &files_uploaded, file_id,
-                        &summary_map](
-                            mf::uploader::UploadStatus status
-                        )
-                    {
-                        boost::apply_visitor(
-                            StatusVisitor( io_service, file_id, files_to_upload,
-                                &files_uploaded, &summary_map),
-                            status.state); });
+                       [&io_service, files_to_upload, &files_uploaded, file_id,
+                        &summary_map](mf::uploader::UploadStatus status)
+                       {
+                           boost::apply_visitor(
+                                   StatusVisitor(io_service, file_id,
+                                                 files_to_upload,
+                                                 &files_uploaded, &summary_map),
+                                   status.state);
+                       });
             }
 
             io_service.run();
@@ -270,20 +265,20 @@ int main(int argc, char *argv[])
                 std::cout << std::endl;
                 for (auto & pair : summary_map)
                 {
-                    std::cout << "File " << (file_id+1) << ": " <<
-                        upload_file_path[file_id] << std::endl;
+                    std::cout << "File " << (file_id + 1) << ": "
+                              << upload_file_path[file_id] << std::endl;
                     std::cout << pair.second << std::endl;
                     ++file_id;
                 }
             }
         }
     }
-    catch(std::exception& e)
+    catch (std::exception & e)
     {
         std::cerr << "Uncaught exception: " << e.what() << "\n";
         return 1;
     }
-    catch(...)
+    catch (...)
     {
         std::cerr << "Exception of unknown type!\n";
         return 1;
