cfd78e50478443d9df2a565e4a177c58f5997486
Herbert Jones <herbert.jones@mediafire.com>
2015-02-26 10:19:36 -0600

    Revert "error field more informative than doupload result"
    This reverts commit 198dd5630236f91b677d1e8934ab1df1a30f8493.
    
    After talking with API developer, the more informative field is the
    doupload/result field.  When there is a doupload error, the error field
    is normally 169, "upload error".

--- src/mediafire_sdk/uploader/detail/transition_upload.hpp
+++ src/mediafire_sdk/uploader/detail/transition_upload.hpp
@@ -31,28 +31,30 @@
 #include "mediafire_sdk/utils/string.hpp"
 #include "mediafire_sdk/utils/url_encode.hpp"
 
-namespace mf {
-namespace uploader {
-namespace detail {
-namespace upload_transition {
+namespace mf
+{
+namespace uploader
+{
+namespace detail
+{
+namespace upload_transition
+{
 
 std::string AsDateTime(std::time_t datetime);
 
-std::string AssembleQuery(
-        const std::map< std::string, std::string > & query_map
-    );
+std::string AssembleQuery(const std::map<std::string, std::string> & query_map);
 
 class TargetUrlVisitor : public boost::static_visitor<>
 {
 public:
-    TargetUrlVisitor(
-        std::map< std::string, std::string > * query_map
-    ) :
-        query_map_(*query_map) {}
+    TargetUrlVisitor(std::map<std::string, std::string> * query_map)
+            : query_map_(*query_map)
+    {
+    }
 
     void operator()(mf::uploader::detail::ParentFolderKey & variant) const
     {
-        if ( ! variant.key.empty() )
+        if (!variant.key.empty())
             query_map_["folder_key"] = variant.key;
     }
 
@@ -62,22 +64,19 @@ public:
     }
 
 private:
-    std::map< std::string, std::string > & query_map_;
+    std::map<std::string, std::string> & query_map_;
 };
 
-class UploadPostDataPipe :
-    public mf::http::PostDataPipeInterface
+class UploadPostDataPipe : public mf::http::PostDataPipeInterface
 {
 public:
-    UploadPostDataPipe(
-            mf::utils::FileIO::Pointer file,
-            const uint64_t start_byte,
-            const uint64_t end_byte
-        ) :
-        file_(file),
-        start_byte_(start_byte),
-        end_byte_(end_byte),
-        current_read_pos_(start_byte_)
+    UploadPostDataPipe(mf::utils::FileIO::Pointer file,
+                       const uint64_t start_byte,
+                       const uint64_t end_byte)
+            : file_(file),
+              start_byte_(start_byte),
+              end_byte_(end_byte),
+              current_read_pos_(start_byte_)
     {
         assert(file_);
 
@@ -86,20 +85,17 @@ public:
         assert(start_byte < end_byte);
     }
 
-    virtual uint64_t PostDataSize() const
-    {
-        return end_byte_ - start_byte_;
-    }
+    virtual uint64_t PostDataSize() const { return end_byte_ - start_byte_; }
 
     virtual mf::http::SharedBuffer::Pointer RetreivePostDataChunk()
     {
-        assert( file_->Tell() == current_read_pos_ );
+        assert(file_->Tell() == current_read_pos_);
 
         if (file_->Tell() != current_read_pos_)
         {
             std::error_code ec;
             file_->Seek(mf::utils::SeekAnchor::Beginning, current_read_pos_,
-                &ec);
+                        &ec);
 
             if (ec)
             {
@@ -154,43 +150,47 @@ private:
 };
 
 template <typename FSM>
-void HandleUploadResponse(
-    FSM & fsm,
-    const std::string & url,
-    const mf::http::Headers & headers,
-    const std::string & content
-)
+void HandleUploadResponse(FSM & fsm,
+                          const std::string & url,
+                          const mf::http::Headers & headers,
+                          const std::string & content)
 {
     mf::api::ResponseBase response;
-    response.InitializeWithContent(
-        url,
-        "",
-        headers,
-        content
-        );
+    response.InitializeWithContent(url, "", headers, content);
 
     if (response.error_code)
     {
         // Prefer doupload error if sent
-        fsm.ProcessEvent(
-                event::Error{response.error_code,
-                             (response.error_string ? *response.error_string
-                                                    : "Upload rejected")});
+        int error = 0;
+        if (mf::api::GetIfExists(response.pt, "response.doupload.result",
+                                 &error))
+        {
+            fsm.ProcessEvent(event::Error{
+                    std::error_code(error, upload_response_category()),
+                    "Upload rejected"});
+        }
+        else
+        {
+            fsm.ProcessEvent(
+                    event::Error{response.error_code,
+                                 (response.error_string ? *response.error_string
+                                                        : "Upload rejected")});
+        }
     }
     else
     {
         // We need the upload key to proceed.
         std::string upload_key;
-        if ( mf::api::GetIfExists( response.pt, "response.doupload.key",
-                &upload_key ) )
+        if (mf::api::GetIfExists(response.pt, "response.doupload.key",
+                                 &upload_key))
         {
-            fsm.ProcessEvent( event::SimpleUploadComplete{upload_key});
+            fsm.ProcessEvent(event::SimpleUploadComplete{upload_key});
         }
         else
         {
             int error = 0;
-            if ( mf::api::GetIfExists( response.pt, "response.doupload.result",
-                    &error ) )
+            if (mf::api::GetIfExists(response.pt, "response.doupload.result",
+                                     &error))
             {
                 fsm.ProcessEvent(event::Error{
                         std::error_code(error, upload_response_category()),
@@ -199,9 +199,8 @@ void HandleUploadResponse(
             else
             {
                 fsm.ProcessEvent(event::Error{
-                    make_error_code(mf::api::api_code::ContentInvalidData),
-                    "Upload response missing uploadkey"
-                    });
+                        make_error_code(mf::api::api_code::ContentInvalidData),
+                        "Upload response missing uploadkey"});
             }
         }
     }
@@ -209,26 +208,21 @@ void HandleUploadResponse(
 
 struct DoSimpleUpload
 {
-    template <typename Event, typename FSM, typename SourceState,
-             typename TargetState>
-    void operator()(
-            Event const &,
-            FSM & fsm,
-            SourceState&,
-            TargetState&
-        )
+    template <typename Event,
+              typename FSM,
+              typename SourceState,
+              typename TargetState>
+    void operator()(Event const &, FSM & fsm, SourceState &, TargetState &)
     {
         Upload(fsm);
     }
 
     template <typename FSM>
-    std::string BuildUrl(
-            FSM & fsm
-        )
+    std::string BuildUrl(FSM & fsm)
     {
         std::string url = "http://www.mediafire.com/api/1.3/upload/simple.php";
 
-        std::map< std::string, std::string > query_map;
+        std::map<std::string, std::string> query_map;
 
         // Action token
         query_map["session_token"] = fsm.ActionToken();
@@ -265,10 +259,7 @@ struct DoSimpleUpload
     }
 
     template <typename FSM>
-    void SetHeaders(
-            mf::http::HttpRequest * http_request,
-            FSM & fsm
-        )
+    void SetHeaders(mf::http::HttpRequest * http_request, FSM & fsm)
     {
         assert(http_request);
 
@@ -286,9 +277,7 @@ struct DoSimpleUpload
     }
 
     template <typename FSM>
-    void Upload(
-            FSM & fsm
-        )
+    void Upload(FSM & fsm)
     {
         auto ec = std::error_code();
         auto file_io = mf::utils::FileIO::Open(fsm.Path(), "rb", &ec);
@@ -314,33 +303,29 @@ struct DoSimpleUpload
 
         auto fsmp = fsm.AsFrontShared();
         auto request = mf::http::HttpRequest::Create(
-            fsm.GetSessionMaintainer()->HttpConfig(),
-            [fsmp, url](mf::http::HttpRequest::CallbackResponse response)
-            {
-                if (response.error_code)
+                fsm.GetSessionMaintainer()->HttpConfig(),
+                [fsmp, url](mf::http::HttpRequest::CallbackResponse response)
                 {
-                    if (response.error_text)
+                    if (response.error_code)
                     {
-                        fsmp->ProcessEvent(event::Error{
-                            response.error_code,
-                            *response.error_text
-                            });
+                        if (response.error_text)
+                        {
+                            fsmp->ProcessEvent(event::Error{
+                                    response.error_code, *response.error_text});
+                        }
+                        else
+                        {
+                            fsmp->ProcessEvent(event::Error{response.error_code,
+                                                            "Unknown error"});
+                        }
                     }
                     else
                     {
-                        fsmp->ProcessEvent(event::Error{
-                            response.error_code,
-                            "Unknown error"
-                            });
+                        HandleUploadResponse(*fsmp.get(), url, response.headers,
+                                             response.content);
                     }
-                }
-                else
-                {
-                    HandleUploadResponse(*fsmp.get(), url, response.headers,
-                        response.content);
-                }
-            },
-            url);
+                },
+                url);
 
         SetHeaders(request.get(), fsm);
 
@@ -353,44 +338,49 @@ struct DoSimpleUpload
 };
 
 template <typename FSM>
-void HandleChunkResponse(
-    uint32_t chunk_id,
-    FSM & fsm,
-    const std::string & url,
-    const mf::http::Headers & headers,
-    const std::string & content
-)
+void HandleChunkResponse(uint32_t chunk_id,
+                         FSM & fsm,
+                         const std::string & url,
+                         const mf::http::Headers & headers,
+                         const std::string & content)
 {
     mf::api::ResponseBase response;
-    response.InitializeWithContent(
-        url,
-        "",
-        headers,
-        content
-        );
+    response.InitializeWithContent(url, "", headers, content);
 
     if (response.error_code)
     {
-        // Upload has unique negative values.
-        fsm.ProcessEvent(
-                event::Error{response.error_code,
-                             (response.error_string ? *response.error_string
-                                                    : "Upload rejected")});
+        // Prefer doupload error
+        int error = 0;
+        if (mf::api::GetIfExists(response.pt, "response.doupload.result",
+                                 &error))
+        {
+            fsm.ProcessEvent(event::Error{
+                    std::error_code(error, upload_response_category()),
+                    "Upload rejected"});
+        }
+        else
+        {
+            // Upload has unique negative values.
+            fsm.ProcessEvent(
+                    event::Error{response.error_code,
+                                 (response.error_string ? *response.error_string
+                                                        : "Upload rejected")});
+        }
     }
     else
     {
         // We need the upload key to proceed.
         std::string upload_key;
-        if ( mf::api::GetIfExists( response.pt, "response.doupload.key",
-                &upload_key ) )
+        if (mf::api::GetIfExists(response.pt, "response.doupload.key",
+                                 &upload_key))
         {
-            fsm.ProcessEvent( event::ChunkSuccess{chunk_id, upload_key});
+            fsm.ProcessEvent(event::ChunkSuccess{chunk_id, upload_key});
         }
         else
         {
             int error = 0;
-            if ( mf::api::GetIfExists( response.pt, "response.doupload.result",
-                    &error ) )
+            if (mf::api::GetIfExists(response.pt, "response.doupload.result",
+                                     &error))
             {
                 fsm.ProcessEvent(event::Error{
                         std::error_code(error, upload_response_category()),
@@ -399,9 +389,8 @@ void HandleChunkResponse(
             else
             {
                 fsm.ProcessEvent(event::Error{
-                    make_error_code(mf::api::api_code::ContentInvalidData),
-                    "Upload response missing uploadkey"
-                    });
+                        make_error_code(mf::api::api_code::ContentInvalidData),
+                        "Upload response missing uploadkey"});
             }
         }
     }
@@ -409,14 +398,11 @@ void HandleChunkResponse(
 
 struct DoChunkUpload
 {
-    template <typename Event, typename FSM, typename SourceState,
-             typename TargetState>
-    void operator()(
-            Event const &,
-            FSM & fsm,
-            SourceState&,
-            TargetState&
-        )
+    template <typename Event,
+              typename FSM,
+              typename SourceState,
+              typename TargetState>
+    void operator()(Event const &, FSM & fsm, SourceState &, TargetState &)
     {
         const boost::optional<uint32_t> next_chunk = fsm.NextChunkToUpload();
         if (next_chunk)
@@ -433,15 +419,12 @@ struct DoChunkUpload
     }
 
     template <typename FSM>
-    std::string BuildUrl(
-            uint32_t /*chunk_id*/,
-            FSM & fsm
-        )
+    std::string BuildUrl(uint32_t /*chunk_id*/, FSM & fsm)
     {
         std::string url
                 = "http://www.mediafire.com/api/1.3/upload/resumable.php";
 
-        std::map< std::string, std::string > query_map;
+        std::map<std::string, std::string> query_map;
 
         // Action token
         query_map["session_token"] = fsm.ActionToken();
@@ -478,15 +461,11 @@ struct DoChunkUpload
     }
 
     template <typename FSM>
-    void SetHeaders(
-            mf::http::HttpRequest * http_request,
-            uint32_t chunk_id,
-            FSM & fsm
-        )
+    void SetHeaders(mf::http::HttpRequest * http_request,
+                    uint32_t chunk_id,
+                    FSM & fsm)
     {
         assert(http_request);
-        assert(fsm.chunkRanges().size() > chunk_id);
-        assert(fsm.chunkHashes().size() > chunk_id);
 
         int begin, end;
         std::tie(begin, end) = fsm.chunkRanges()[chunk_id];
@@ -517,16 +496,13 @@ struct DoChunkUpload
 
         { /* x-unit-size */
             std::ostringstream ss;
-            ss << (end-begin);
+            ss << (end - begin);
             http_request->SetHeader("x-unit-size", ss.str());
         }
     }
 
     template <typename FSM>
-    void UploadNextChunk(
-            const uint32_t chunk_id,
-            FSM & fsm
-        )
+    void UploadNextChunk(const uint32_t chunk_id, FSM & fsm)
     {
         auto ec = std::error_code();
         auto file_io = mf::utils::FileIO::Open(fsm.Path(), "rb", &ec);
@@ -555,33 +531,30 @@ struct DoChunkUpload
 
         auto fsmp = fsm.AsFrontShared();
         auto request = mf::http::HttpRequest::Create(
-            fsm.GetSessionMaintainer()->HttpConfig(),
-            [fsmp, chunk_id, url](mf::http::HttpRequest::CallbackResponse response)
-            {
-                if (response.error_code)
+                fsm.GetSessionMaintainer()->HttpConfig(),
+                [fsmp, chunk_id, url](
+                        mf::http::HttpRequest::CallbackResponse response)
                 {
-                    if (response.error_text)
+                    if (response.error_code)
                     {
-                        fsmp->ProcessEvent(event::Error{
-                            response.error_code,
-                            *response.error_text
-                            });
+                        if (response.error_text)
+                        {
+                            fsmp->ProcessEvent(event::Error{
+                                    response.error_code, *response.error_text});
+                        }
+                        else
+                        {
+                            fsmp->ProcessEvent(event::Error{response.error_code,
+                                                            "Unknown error"});
+                        }
                     }
                     else
                     {
-                        fsmp->ProcessEvent(event::Error{
-                            response.error_code,
-                            "Unknown error"
-                            });
+                        HandleChunkResponse(chunk_id, *fsmp.get(), url,
+                                            response.headers, response.content);
                     }
-                }
-                else
-                {
-                    HandleChunkResponse(chunk_id, *fsmp.get(), url,
-                        response.headers, response.content);
-                }
-            },
-            url);
+                },
+                url);
 
         SetHeaders(request.get(), chunk_id, fsm);
 
