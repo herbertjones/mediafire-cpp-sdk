7e8b4479a49914458926f3b25452f6c50f3347be
Herbert Jones <herbert.jones@mediafire.com>
2015-07-07 07:49:52 -0500

    Add get_changes 1.3

--- src/mediafire_sdk/api/device/get_changes.hpp
+++ src/mediafire_sdk/api/device/get_changes.hpp
@@ -9,6 +9,7 @@
 #pragma once
 
 #include "get_changes/v0.hpp"
+#include "get_changes/v1_3.hpp"
 
 namespace mf {
 namespace api {
@@ -18,7 +19,7 @@ namespace device {
 namespace get_changes {
 
 // Default version
-using namespace v0;  // NOLINT
+using namespace v1_3;  // NOLINT
 
 }  // namespace get_changes
 }  // namespace device
